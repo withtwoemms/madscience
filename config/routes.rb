58d8d69c09069c725e1a1ed0933a6018b939f86b
Emmanuel I. Obi <withtwoemms@gmail.com>
2015-07-14 10:24:15 -0500

    partial commit to resolve merge conflict

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,18 @@ Rails.application.routes.draw do
   resources :projects do
     resources :experiments, except: :index
   end
+
+  resources :users
+
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'sessions#new' # TEMPORARY
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
