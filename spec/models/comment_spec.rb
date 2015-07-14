63079a8c12c3d12335cd1a4456ec45961c4f60e6
Joe Awad <joeaawad@gmail.com>
2015-07-14 18:51:46 -0500

    add comment tests

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -13,5 +13,67 @@ describe Comment do
     it "should belong to a project" do
       expect(project_comment.commentable.class.name).to eq("Project")
     end
+
+    it "should have content" do
+      expect(project_comment).to have_attribute(:content)
+    end
+
+    it "should have commenter" do
+      expect(project_comment.commenter.class.name).to eq("User")
+    end
+  end
+
+  describe 'Experiment Comment' do
+    it "Should belong to the Comment class" do
+      expect(experiment_comment.class.name).to eq("Comment")
+    end
+
+    it "should belong to an experiment" do
+      expect(experiment_comment.commentable.class.name).to eq("Experiment")
+    end
+
+    it "should have content" do
+      expect(experiment_comment).to have_attribute(:content)
+    end
+
+    it "should have commenter" do
+      expect(experiment_comment.commenter.class.name).to eq("User")
+    end
+  end
+
+  describe 'Observed Experiment Comment' do
+    it "Should belong to the Comment class" do
+      expect(observed_experiment_comment.class.name).to eq("Comment")
+    end
+
+    it "should belong to an observation" do
+      expect(observed_experiment_comment.commentable.class.name).to eq("Observation")
+    end
+
+    it "should have content" do
+      expect(observed_experiment_comment).to have_attribute(:content)
+    end
+
+    it "should have commenter" do
+      expect(observed_experiment_comment.commenter.class.name).to eq("User")
+    end
+  end
+
+  describe 'Observed Procedure Comment' do
+    it "Should belong to the Comment class" do
+      expect(observed_procedure_comment.class.name).to eq("Comment")
+    end
+
+    it "should belong to an observation" do
+      expect(observed_procedure_comment.commentable.class.name).to eq("Observation")
+    end
+
+    it "should have content" do
+      expect(observed_procedure_comment).to have_attribute(:content)
+    end
+
+    it "should have commenter" do
+      expect(observed_procedure_comment.commenter.class.name).to eq("User")
+    end
   end
 end
