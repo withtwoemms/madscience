14a991c090315c66bfb3471fefe3991309308cc3
Jason Pettus <ilikejason@gmail.com>
2015-07-15 19:40:48 -0500

    Fix links for all tabs in experiments/show page
    Routes were wrong for all the various tabs on the experiments/show
    view page, plus the links on the page itself. All are fixed and
    working now.

--- config/routes.rb
+++ config/routes.rb
@@ -18,10 +18,10 @@ Rails.application.routes.draw do
       resources :comments, except: [:index, :show, :create]
         post '/comments', to: 'comments#experiment_create'
     end
-    get '/conclusions', to: 'experiments#conclusions'
-    get '/results', to: 'experiments#results'
-    get '/observations', to: 'experiments#observations'
-    get '/procedures', to: 'experiments#procedures'
+    get '/experiments/:id/conclusions', to: 'experiments#conclusions', as: 'conclusions'
+    get '/experiments/:id/results', to: 'experiments#results', as: 'results'
+    get '/experiments/:id/observations', to: 'experiments#observations', as: 'observations'
+    get '/experiments/:id/procedures', to: 'experiments#procedures', as: 'procedures'
     resources :comments, except: [:index, :show, :create]
       post '/comments', to: 'comments#project_create'
   end
